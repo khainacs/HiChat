4b1cb0442453353f5539c78a3f0efa8ce66a22ef
khainacs <87764594+khainacs@users.noreply.github.com>
2024-10-04 12:19:52 +0700

    Revert "Revert "feat: create model and db""

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,31 @@
-const express = require('express');
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import sequelize from "./database.js"
+import UserModel from './models/usermodel.js'; 
+import MessageModel from './models/messageModel.js';
+import ConversationModel from './models/conversationModel.js';
 const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}));
+app.use(cors());
+app.use(express.static('public'));
+app.use('/imgaes', express.static('images'));
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+sequelize
+.sync()
+.then(() => {
+console.log('Kết nối đến cơ sở dữ liệu thành công.');
+app.listen(PORT, () => {
+    console.log(`Listening at Port ${PORT}`);
+});
+})
+.catch((error) => {
+console.error('Kết nối thất bại:', error);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
